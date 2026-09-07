38afe7bf12067401b7b03187d128e44096e90693
Khalid Ayomide <khalidayomide50@gmail.com>
2026-09-07 01:52:54 +0100

    feat(backend): add models package init

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+"""SQLAlchemy models."""
+
+# from app.models.customer import Customer
+# from app.models.lead import Lead
+# from app.models.conversation import Conversation, Message
+# from app.models.followup import FollowUp
+# from app.models.sales_agent import SalesAgent
