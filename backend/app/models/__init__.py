210db442713e364c12f8a7a5a19f5d2f7dec6ee0
Khalid Ayomide <khalidayomide50@gmail.com>
2026-09-08 07:37:43 +0100

    feat(backend): export all models from models package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,16 @@
 """SQLAlchemy models."""
 
-# from app.models.customer import Customer
-# from app.models.lead import Lead
-# from app.models.conversation import Conversation, Message
-# from app.models.followup import FollowUp
-# from app.models.sales_agent import SalesAgent
+from app.models.customer import Customer
+from app.models.lead import Lead
+from app.models.conversation import Conversation, Message
+from app.models.sales_agent import SalesAgent
+
+# from app.models.followup import FollowUp  # TODO
+
+__all__ = [
+    "Customer",
+    "Lead",
+    "Conversation",
+    "Message",
+    "SalesAgent",
+]
